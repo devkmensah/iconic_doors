69cab966b8990523ee6c50c4834cdf97919b0a94
wolfie Dedev <wolfiededev@kodeds-iMac.localdomain>
2021-03-04 20:55:44 +0000

    coded reveal sections on scroll functionality for the home page

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ const consultation = document.querySelector('#consultation');
 const arrowObserver = new IntersectionObserver(
   function (entries) {
     const [entry] = entries;
+    // console.log(entry);
 
     if (entry.isIntersecting) arrowUP.classList.add('hidden');
     else arrowUP.classList.remove('hidden');
@@ -38,3 +39,24 @@ const arrowObserver = new IntersectionObserver(
 );
 arrowObserver.observe(consultation);
 // intersecting means when the target being observed appears or touches the rootmargin of the entire page. The root margin is the head or top of the page. If a root margin of 90px is set, that'll be 90px margin from the top of the page to wherever it meets our target.
+
+// 4... Reveal Sections on scroll for home page
+const allSections = document.querySelectorAll('.reveal-section');
+
+const sectionObserver = new IntersectionObserver(
+  function (entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('unreveal');
+    observer.unobserve(entry.target);
+  },
+  {
+    root: null,
+    threshold: 0.14,
+  }
+);
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('unreveal');
+});
