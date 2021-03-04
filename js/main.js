5ee5894e4f09d63f92e39376ee799438f7182f97
wolfie Dedev <wolfiededev@kodeds-iMac.localdomain>
2021-03-04 20:23:01 +0000

    coded hide & show functionality for the footer arrow btn

--- js/main.js
+++ js/main.js
@@ -2,16 +2,17 @@
 
 'use strict';
 
-// 1. Start Now Btn Smooth scroll effect
+// 1... Start Now Btn Smooth scroll effect
 const startBtn = document.querySelector('.startBtn ');
 const scheduleForm = document.querySelector('.consultation-form');
 
 startBtn.addEventListener('click', function (e) {
   e.preventDefault();
+
   scheduleForm.scrollIntoView({ behavior: 'smooth' });
 });
 
-// 2. Arrow up Btn Smooth scroll effect
+// 2... Arrow up Btn Smooth scroll effect
 const arrowUP = document.querySelector('.arrow-up ');
 const header = document.querySelector('#header');
 
@@ -19,3 +20,21 @@ arrowUP.addEventListener('click', function (e) {
   e.preventDefault();
   header.scrollIntoView({ behavior: 'smooth' });
 });
+
+// 3... Show arrow up btn
+const consultation = document.querySelector('#consultation');
+const arrowObserver = new IntersectionObserver(
+  function (entries) {
+    const [entry] = entries;
+
+    if (entry.isIntersecting) arrowUP.classList.add('hidden');
+    else arrowUP.classList.remove('hidden');
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: `${0}px`,
+  }
+);
+arrowObserver.observe(consultation);
+// intersecting means when the target being observed appears or touches the rootmargin of the entire page. The root margin is the head or top of the page. If a root margin of 90px is set, that'll be 90px margin from the top of the page to wherever it meets our target.
